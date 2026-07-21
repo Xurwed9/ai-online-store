847bde9ca55bd86529383e3301055b680bac3ada
Xurshed <xursheddddddd@gmail.com>
2026-07-21 11:32:03 +0500

    feat(models): add User ORM model

---
+++ app/models/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+from database.database import Base
+
+class User(Base):
+    __tablename__ = 'users'
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    username: Mapped[str] = mapped_column(String(100),unique=True)
+    email: Mapped[str] = mapped_column(String(100), unique=True)
+    password: Mapped[str] = mapped_column(String)
+    phone_number: Mapped[str] = mapped_column(String(20),unique=True)
