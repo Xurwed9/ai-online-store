2adc323afa118fb2adea4aeb377f12e4c986f4ae
Xurshed <xursheddddddd@gmail.com>
2026-07-21 15:25:23 +0500

    feat: add email verification with SMTP and database migration

--- app/models/models.py
+++ app/models/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Integer, String
+from sqlalchemy import Integer, String,Boolean
 from sqlalchemy.orm import Mapped, mapped_column
 from app.database.database import Base
 
@@ -10,3 +10,5 @@ class User(Base):
     password: Mapped[str] = mapped_column(String)
     phone_number: Mapped[str] = mapped_column(String(20),unique=True)
     role: Mapped[str] = mapped_column(String(50), default='user')
+    is_verified: Mapped[bool] = mapped_column(Boolean, default=False)
+    verification_code: Mapped[str] = mapped_column(String, nullable=True)
