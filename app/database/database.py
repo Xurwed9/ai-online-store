c2634e2b891a97806ec2792e0c551db35c48630b
Xurshed <xursheddddddd@gmail.com>
2026-07-21 11:21:10 +0500

    first commit

---
+++ app/database/database.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+
+AsyncSessionLocal = async_sessionmaker(engine)
+
+async def get_db():
+  async with AsyncSessionLocal() as session:
+    yield session
+
+class Base(DeclarativeBase):
+  pass
