d506add1e08311c132ac3f468585b8d1a5764bb8
Xurshed <xursheddddddd@gmail.com>
2026-07-21 11:41:24 +0500

    feat(schemas): add user pydantic validation schemas

---
+++ app/schemas/user.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel,EmailStr,Field
+
+class UserCreate(BaseModel):
+    username: str = Field(min_length=5, max_length=30)
+    email: EmailStr
+    password: str = Field(min_length=6)
+    phone_number: str
+
+class UserResponse(BaseModel):
+    id: int
+    username: str
+    email: str
+    phone_number: str
+
+class UserLogin(BaseModel):
+    username: str
+    password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
