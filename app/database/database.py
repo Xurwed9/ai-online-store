05a810240511f602e8bff4f416ff3df2efb98086
Xurshed <xursheddddddd@gmail.com>
2026-09-19 16:50:27 +0500

    fix database url

--- app/database/database.py
+++ app/database/database.py
@@ -7,6 +7,13 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+if DATABASE_URL.startswith("postgresql://"):
+    DATABASE_URL = DATABASE_URL.replace(
+        "postgresql://",
+        "postgresql+asyncpg://",
+        1
+    )
+
 engine = create_async_engine(DATABASE_URL, echo=False)
 
 AsyncSessionLocal = async_sessionmaker(engine)
